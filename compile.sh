a7eb682d8d4c8b8112dc1fc008c8e54deb437d31
Daniel Ribeiro <danrbr@gmail.com>
2013-09-21 23:43:23 -0700

    create gh-pages

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo Watching changes in coffee
-rm -rf ./lib/*
-coffee -w -o lib/ -c coffee/
-
--- package.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo Distributing the extension...
-rm -rf ./lib/*
-coffee -o lib/ -c coffee/
-mkdir -p pkg/
-zip -r pkg/three-hub.zip . -x '.*/*' '.*' '*.sh' 'coffee/*' 'test/*' 'pkg/*' 'docs/*'
-echo Distribution file created at pkg/three-hub.zip
